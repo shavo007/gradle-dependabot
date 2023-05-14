34346fa054d7af08c5db5d184f39e585ec12ca6a
shane <shanelee007@gmail.com>
2023-05-14 21:36:48 +1000

    initial commit

---
+++ src/main/java/com/example/gradledependabot/GradleDependabotApplication.java
@@ -0,0 +1,13 @@
+package com.example.gradledependabot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradleDependabotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GradleDependabotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/gradledependabot/GradleDependabotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gradledependabot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GradleDependabotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
